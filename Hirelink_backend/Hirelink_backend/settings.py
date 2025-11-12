c0368bc0886eaf8c85a8ea67acce51eab393e221
Asma Hammami <hammamiasma52@gmail.com>
2025-11-12 22:28:40 +0100

    backend: implement user authentication system

--- Hirelink_backend/Hirelink_backend/settings.py
+++ Hirelink_backend/Hirelink_backend/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#For media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'rest_framework',
     'corsheaders',
     'django.contrib.admin',
@@ -78,10 +83,19 @@ WSGI_APPLICATION = 'Hirelink_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hirelink_db',
+        'USER': 'root',           
+        'PASSWORD': 'root',    
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4'
+        },
     }
 }
+
 
 
 # Password validation
@@ -131,3 +145,4 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+AUTH_USER_MODEL = 'users.CustomUser'
